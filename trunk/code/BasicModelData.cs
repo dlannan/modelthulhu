6e973f4f04420f8269b9ad378d6c5b8e4a9b70c7
andrew.lubitz <andrew.lubitz@a069f3f9-e442-ad2d-d3e9-dd8107560431>
2010-05-30 23:10:47 +0000

    Added BasicModelData.cs ... it's got only the bare minimum needed to make the project build. All of the functions and stuff from the 'home-brew' version have been stripped out.
    Added a using directive in ModelUtil.cs

---
+++ trunk/code/BasicModelData.cs
@@ -0,0 +1,14 @@
+﻿namespace Modelthulhu.UserModel
+{
+    // Basic model data class
+    // Contains 3-component verts, 2-component texture coords, and 3-component vertex normals; each triangle has 3 of each
+    public class BasicModelData
+    {
+        // parallel vertex attrib arrays
+        public double[] x, y, z;                // vertices
+        public double[] u, v;                   // texture coordinates
+        public double[] nx, ny, nz;             // normal vectors
+        // triangle vertex attrib indices
+        public uint[] a_vert, a_uv, a_norm, b_vert, b_uv, b_norm, c_vert, c_uv, c_norm;
+    }
+}
